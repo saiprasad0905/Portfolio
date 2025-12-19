3e26cad6455425d7e5a57447941073f9325b989e
saiprasad k <saiprasadkathiresan04@gmail.com>
2025-12-19 13:48:59 +0530

    Fixed errors in contacts page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => (
   <section id="contact" className="contact">
     <h2>Contact</h2>
     <p>Email: <a href="mailto:youremail@example.com">saiprasadkathiresan04@gmail.com</a></p>
-    <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noreferrer">www.linkedin.com/in/sai-prasad-kathiresan-475263299</a></p>
-    <p>GitHub: <a href="https://github.com/yourusername" target="_blank" rel="noreferrer">https://github.com/saiprasad0905</a></p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/sai-prasad-kathiresan/" target="_blank" rel="noreferrer">www.linkedin.com/in/sai-prasad-kathiresan-475263299</a></p>
+    <p>GitHub: <a href="https://github.com/saiprasad0905" target="_blank" rel="noreferrer">https://github.com/saiprasad0905</a></p>
   </section>
 );
 
