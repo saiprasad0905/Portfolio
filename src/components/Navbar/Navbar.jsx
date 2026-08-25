811262634d6c7dec3bacd8298f5cf182c07bd87a
saiprasad k <saiprasadkathiresan04@gmail.com>
2026-08-25 13:06:04 +0530

    Introduced a new section resume

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => (
   <li><a href="#certifications">Certifications</a></li>
   <li><a href="#projects">Projects</a></li>
   <li><a href="#contact">Contact</a></li>
+  <li><a href="#resume">Resume</a></li>
 </ul>
   </nav>
 );
